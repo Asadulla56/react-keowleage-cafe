083ebc31c2617124ac1fa823253cef30c65977ab
Asadulla56 <mdasadullahoque294@gmail.com>
2024-03-15 00:15:10 +0600

    added the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './compontents/Header/Header'
+import Blogs from './compontents/Blogs/Blogs'
+import Booksmarks from './compontents/Booksmarks/Booksmarks'
 
 function App() {
 
   return (
     <>
       <Header></Header>
+      <div className='md:flex'>
+      <Blogs></Blogs>
+      <Booksmarks></Booksmarks>
+      </div>
+      <div>
+
+      </div>
     </>
   )
 }
